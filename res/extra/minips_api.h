afc233867966b21438f6caa3ade9274025d1482e
Edu Renesto <edure95@gmail.com>
2021-03-29 13:03:11 -0300

    extra: atualiza experimentos antes de comecar parte 2

--- res/extra/minips_api.h
+++ res/extra/minips_api.h
@@ -1,55 +1,9 @@
-// Ainda não consigo linkar mais de um arquivo junto...
-// Então, a API inteira vai num .h só mesmo. Tudo static pra não
-// dar problema.
+extern void minips_print_int(int i);
 
-static void minips_print_int(int i) {
-    asm volatile(
-        "addi $v0, $zero, 1;"
-        "add $a0, $zero, %[i];"
-        "syscall" 
-        : 
-        : [i] "r" (i)
-        : "$a0", "$v0");
-}
+extern void minips_print_char(char c);
 
-static void minips_print_char(char c) {
-    asm volatile(
-        "addi $v0, $zero, 11;"
-        "add $a0, $zero, %[c];"
-        "syscall" 
-        : 
-        : [c] "r" (c)
-        : "$a0", "$v0");
-}
+extern void minips_print_str(const char* str);
 
-static void minips_print_str(const char* str) {
-    asm volatile(
-        "addi $v0, $zero, 4;"
-        "add $a0, $zero, %[str];"
-        "syscall" 
-        : 
-        : [str] "r" (str)
-        : "$a0", "$v0");
-}
+extern int minips_read_int();
 
-static int minips_read_int() {
-    int a;
-    asm volatile(
-        "addi $v0, $zero, 5;"
-        "syscall;"
-        "add %[a], $v0, $zero;"
-        : [a] "=r" (a)
-        :
-        : "$v0");
-    
-    return a;
-}
-
-static void minips_halt() {
-    asm volatile(
-        "addi $v0, $zero, 10;"
-        "syscall"
-        :
-        :
-        : "$v0");
-}
+extern void minips_halt();
